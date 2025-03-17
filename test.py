a2dbd2eebdbecb2ee63868607b3b1c6e07a4fa97
Luca Menon <184136845+B0T1o1@users.noreply.github.com>
2025-03-17 12:32:45 +0000

    Update test.py

--- test.py
+++ test.py
@@ -10,8 +10,8 @@ data = load_digits()
 X = data['data']
 Train_test_split = 0.8
 n = int(Train_test_split*len(X))
-X_train = X[n:]
-X_test = X[:n]
+X_train = X[:n]
+X_test = X[n:]
 Y = data['target']
 
 CompeletedY = []
@@ -20,18 +20,21 @@ for y in Y:
     putin[y] = 1
     CompeletedY.append(putin)
 layers = [63,32,16,10]
-network = NN.NeuralNetwork(NN.TanH,NN.MeanSqauredError,0.01,64,*layers)
+network = NN.NeuralNetwork(NN.TanH(),NN.MeanSquaredError(),0.1,64,*layers)
 
-y_train = CompeletedY[n:]
-y_test = CompeletedY[:n]
+y_train = CompeletedY[:n]
+y_test = CompeletedY[n:]
 
-network.train(X_train,y_train,100)
+
+
+network.train(X_train,y_train,10)
 
 correct = 0
-for idata in range(X_test):
+for idata in range(len(X_test)):
     result = network.forward(X_test[idata])
     if np.argmax(result) == np.argmax(y_test[idata]):
         correct += 1
+    else: print(result)
 print(correct/len(X_test))
 
 
